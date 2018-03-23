3509ba8d84b70ef40c99647df8b8c07625ee86aa
HelpSeeker <36918959+HelpSeeker@users.noreply.github.com>
2018-03-23 16:44:35 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # RestrictedWebm
-A simple shell script to create webms within a certain file size limit (mainly targeted at 4chan). The main focus lies on forcing the file size within the specified limit while still achieving the maximum possible quality.
+A simple shell script to create webms within a certain file size limit (mainly targeted at 4chan).
+
+The main goal is to produce webms that fit within a specified file limit, while producing the maximum possible quality and requiring minimum user input. If you want fast encoding speed, then this script isn't for you.
 
 ```
 Usage: sh convert.sh [-h] [-t] [-a] [-p] [-m { variable | constant | low-variable }] [-s file_size_limit]
@@ -11,14 +13,53 @@ Usage: sh convert.sh [-h] [-t] [-a] [-p] [-m { variable | constant | low-variabl
 		variable (default): Usually provides the best quality/size ratio. Works with target/minimum crf and bitrate value.
 		constant: Easiest way to reach a certain file size. Specifies an average target bitrate.
 		low-variable: Same as the "variable" mode, but doesn't apply a minimum crf value. Only use for long webms, if "variable" produces far too large files.
-	-s file_size_limit: Specifies the file size limit in MB. Default value is 4.
+	-s file_size_limit: Specifies the file size limit in MB. Default value is 3.
 		4chan limits:
 			/gif/ and /wsg/: 4MB - audio allowed - max. 300 seconds
 			all other boards: 3MB - no audio allowed - max. 120 seconds
 		8chan limits:
 			all boards: 8MB - audio allowed
+	-f filters: Add filters that you want to apply (with settings). Be careful to type them as you would normally with ffmpeg. Refer to ffmpeg's documentation for further information.
+
 ```
 
 **Requirements:**  
 ffmpeg  
 ffprobe
+```
+Folder structure:
+
+RestrictedWebm/
+│
+├── convert.sh
+│
+├── to_convert/
+      │ 
+      │ video01
+      │ video02
+      │ video03
+      │ ...
+
+```
+
+**Functions:**
+
+General:  
+- [x] Convert all videos from the to_convert folder into webms  
+- [x] Specify which file size limit to adhere to (default: 3MB)  
+- [x] Trim each video individually (default: off)  
+- [x] Apply filters to all videos  
+- [ ] Set filters for individual videos
+- [x] Use each video's file name for the title metadata  
+- [ ] Option to switch from VP8/Vorbis to VP9/Opus  
+
+Quality adjustments:  
+- [x] Use 2-pass encoding (default: off)  
+- [x] Choose which bitrate mode to use (default: VBR)  
+- [x] Calculate bitrate based on video length  
+- [ ] Loops through several settings (bitrate modes, skip_threshold, ...) until the file size fits in the specified limit  
+- [ ] Automatic downscaling if quality would be too low otherwise  
+
+Audio:  
+- [x] Encode with audio (default: off)  
+- [ ] Adjust audio bitrate based on the video length  
--- convert.sh
+++ convert.sh
@@ -5,25 +5,26 @@ cd to_convert || exit
 
 # Define help text
 usage () {
-echo -e "Usage: sh $0 [-h] [-t] [-a] [-p] [-m { variable | constant | low-variable }] [-s file_size_limit]"
-echo -e "\t-h: Show help"
+echo -e "Usage: sh $0 [-h] [-t] [-a] [-p] [-m { variable | constant | low-variable }] [-s file_size_limit] [-f filters]"
+echo -e "\t-h: Show Help"
 echo -e "\t-t: Enable trim mode. Lets you specify which part of the input video(s) to encode"
 echo -e "\t-a: Enables audio encoding"
-echo -e "\t-p: Enables VP8's two pass encoding. Only recommended for high bitrates (e.i. short webms)"
+echo -e "\t-p: Enables VP8's 2-pass encoding. Only recommended for high bitrates (e.i. short webms)"
 echo -e "\t-m { variable | constant | low-variable }: Specifies which bitrate mode VP8 should use"
 echo -e "\t\tvariable (default): Usually provides the best quality/size ratio. Works with target/minimum crf and bitrate value."
 echo -e "\t\tconstant: Easiest way to reach a certain file size. Specifies an average target bitrate."
 echo -e "\t\tlow-variable: Same as the \"variable\" mode, but doesn't apply a minimum crf value. Useful for low-quality webms."
-echo -e "\t-s file_size_limit: Specifies the file size limit in MB. Default value is 4."
+echo -e "\t-s file_size_limit: Specifies the file size limit in MB. Default value is 3."
 echo -e "\t\t4chan limits:"
 echo -e "\t\t\t/gif/ and /wsg/: 4MB - audio allowed - max. 300 seconds"
 echo -e "\t\t\tall other boards: 3MB - no audio allowed - max. 120 seconds"
 echo -e "\t\t8chan limits:"
 echo -e "\t\t\tall boards: 8MB - audio allowed"
+echo -e "\t-f filters: Add filters that you want to apply (with settings). Be careful to type them as you would normally with ffmpeg. Refer to ffmpeg's documentation for further information."
 }
  
 # Reads input parameters and assigns values accordingly
-while getopts ":htapm:s:" ARG;
+while getopts ":htapm:s:f:" ARG;
 do case "$ARG" in
 h) usage && exit;;
 t) trim_mode=true;;
@@ -31,6 +32,8 @@ a) audio_mode=true;;
 p) two_pass=true;;
 m) bitrate_mode="$OPTARG";;
 s) file_size=$OPTARG;;
+f) filter_settings="-filter_complex $OPTARG";;
+\?) echo "Unknown flag used. Enter sh $0 -h to show all available options." && exit;;
 esac;
 done
 
@@ -39,13 +42,14 @@ done
 [[ -z $audio_mode ]] && audio_mode=false
 [[ -z $two_pass ]] && two_pass=false
 [[ -z $bitrate_mode ]] && bitrate_mode="variable"
-[[ -z $file_size ]] && file_size=4
+[[ -z $file_size ]] && file_size=3
 
 # The main conversion loop
 for input in *; do (
 	
-	# Find out length of the video for further calculations
+	# Use ffprobe to get video properties for later calculations
 	length=$(ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input")
+	frame_rate=$(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=noprint_wrappers=1:nokey=1 "$input")
 	
 	# Trim settings
 	if [[ "$trim_mode" = true ]]; then
@@ -88,10 +92,10 @@ for input in *; do (
 	# Use ffmpeg to convert to webm
 	if [[ "$two_pass" = true ]]; then
 		ffmpeg -y -ss $start_time -i "$input" -t $duration -c:v libvpx -slices 8 -threads 1 $video_settings -deadline good -cpu-used 5 $audio_settings -pass 1 -f webm /dev/null
-		ffmpeg -y -ss $start_time -i "$input" -t $duration -c:v libvpx -slices 8 -threads 1 -metadata title="${input%.*}" -auto-alt-ref 1 -lag-in-frames 16 -bufsize $bufsize $video_settings -deadline best -cpu-used 0 $audio_settings -pass 2 "../${input%.*}.webm"
+		ffmpeg -y -ss $start_time -i "$input" -t $duration -c:v libvpx -slices 8 -threads 1 -metadata title="${input%.*}" -auto-alt-ref 1 -lag-in-frames 16 -bufsize $bufsize $video_settings -deadline best -cpu-used 0 $filter_settings $audio_settings -pass 2 "../${input%.*}.webm"
 		rm ffmpeg2pass-0.log
 	else
-		ffmpeg -y -ss $start_time -i "$input" -t $duration -c:v libvpx -slices 8 -threads 1 -metadata title="${input%.*}" -lag-in-frames 16 -bufsize $bufsize $video_settings -deadline best -cpu-used 0 $audio_settings "../${input%.*}.webm"
+		ffmpeg -y -ss $start_time -i "$input" -t $duration -c:v libvpx -slices 8 -threads 1 -metadata title="${input%.*}" -lag-in-frames 16 -bufsize $bufsize $video_settings -deadline best -cpu-used 0 $filter_settings $audio_settings "../${input%.*}.webm"
 	fi
 	
 ); done
