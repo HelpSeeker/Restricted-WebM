085f68238b43c1d10c72fefad35ef7f1dee38294
HelpSeeker <36918959+HelpSeeker@users.noreply.github.com>
2018-03-25 23:54:06 +0200

    Add files via upload
    Custom buffer size now only gets utilized during the last encoding attempt (together with skip_threshold) in order to brute force any video into the specified limit.  
    Added option to use VP9/Opus instead of VP8/Vorbis.  
    Scale can now again be used with the -f flag. If the user sets the scale filter manually, automatic downscaling will be disabled.

--- README.md
+++ README.md
@@ -1,37 +1,38 @@
-# RestrictedWebm
-A simple shell script to create webms within a certain file size limit (mainly targeted at 4chan).
+# Restricted-Webm
+A shell script to create webms within a certain file size limit (mainly targeted at 4chan).
 
 The main goal is to produce webms that fit within a specified size limit, while producing the maximum possible quality and requiring minimum user input. If you want fast encoding speed, then this script isn't for you.  
 
 **How it works:**  
 
 1. Calculates video bitrate based on the (trimmed) input video length and whether or not audio mode is active.  
-2. Downscales the video to ensure a minimum bits per pixel value (>= 0.04) is reached. Stops at 360p, even if bpp < 0.04.
+2. Downscales the video to ensure a minimum bits per pixel value (>= 0.04) is reached. Stops at 360p, even if bpp < 0.04. Automatic downscaling is disabled if the scale filter is used manually.  
 3. Encodes a webm with variable bitrate mode and a minimum crf value. Uses 2-pass encoding if bits per pixel value is high enough (>= 0.075).  
 4. IF the file size of the new webm is larger than the specified limit, it tries again with different settings (variable bitrate without minimum crf -> constant bitrate -> constant bitrate and allows ffmpeg to drop frames).  
 
 ```
-Usage: convert.sh [-h] [-t] [-a] [-s file_size_limit]
+Usage: convert.sh [-h] [-t] [-a] [-n] [-s file_size_limit]
 	-h: Show help
 	-t: Enable trim mode. Lets you specify which part of the input video(s) to encode
 	-a: Enables audio encoding
+	-n: Use the newer codecs VP9/Opus instead of VP8/Vorbis. Will lead to even longer encoding times, but offers a better quality (especially at low bitrates). Also note that 4chan doesn't support VP9/Opus webms.  
 	-s file_size_limit: Specifies the file size limit in MB. Default value is 3.
 		4chan limits:
 			/gif/ and /wsg/: 4MB - audio allowed - max. 300 seconds
 			all other boards: 3MB - no audio allowed - max. 120 seconds
 		8chan limits:
 			all boards: 8MB - audio allowed
-	-f filters: Add filters that you want to apply (with settings). DO NOT USE SCALING AS IT'S APPLIED AUTOMATICALLY RIGHT NOW! Also be careful to type them as you would normally with ffmpeg. Refer to ffmpeg's documentation for further information.
+	-f filters: Add filters that you want to apply (with settings). Be careful to type them as you would normally with ffmpeg. Refer to ffmpeg's documentation for further information.
 
 ```
 
 **Requirements:**  
 ffmpeg  
-ffprobe
+ffprobe  
 ```
 Folder structure:
 
-RestrictedWebm/
+Restricted-Webm/
 │
 ├── convert.sh
 │
@@ -48,19 +49,19 @@ RestrictedWebm/
 
 General:  
 - [x] Convert all videos from the to_convert folder into webms  
-- [x] Specify which file size limit to adhere to (default: 3MB)  
+- [x] Specify which file size limit to adhere to (default: 3MB)
+- [x] Option to use VP9/Opus instead of VP8/Vorbis  
 - [x] Trim each video individually (default: off)  
 - [x] Apply filters to all videos  
 - [x] Use each video's file name for the title metadata  
 - [ ] Set filters for individual videos  
-- [ ] Option to switch from VP8/Vorbis to VP9/Opus  
 
 Quality adjustments:  
 - [x] Calculate bitrate based on video length  
-- [x] Automatic downscaling if quality would be too low otherwise  
+- [x] Automatic downscaling if quality would be too low otherwise (disabled when using scale manually)
 - [x] Use 2-pass encoding automatically  
 - [x] Loops through bitrate settings until file size fits into the specified limit  
-- [ ] Offer HighQuality mode, where low quality output is prevented by demanding additional input from the user
+- [ ] HighQuality mode, where low quality output is prevented by demanding additional input from the user
 
 Audio:  
 - [x] Encode with audio (default: off)  
--- convert.sh
+++ convert.sh
@@ -4,22 +4,32 @@
 ###### Functions ######
 ###################
 
-# Define help text
+# Defines help text
 usage () {
-	echo -e "Usage: $0 [-h] [-t] [-a] [-s file_size_limit] [-f filters]"
+	echo -e "Usage: $0 [-h] [-t] [-a] [-n] [-s file_size_limit] [-f filters]"
 	echo -e "\\t-h: Show Help"
 	echo -e "\\t-t: Enable trim mode. Lets you specify which part of the input video(s) to encode"
 	echo -e "\\t-a: Enables audio encoding"
+	echo -e "\\t-n: Use the newer codecs VP9/Opus instead of VP8/Vorbis. Will lead to even longer encoding times, but offers a better quality (especially at low bitrates). Also note that 4chan doesn't support VP9/Opus webms."
 	echo -e "\\t-s file_size_limit: Specifies the file size limit in MB. Default value is 3."
 	echo -e "\\t\\t4chan limits:"
 	echo -e "\\t\\t\\t/gif/ and /wsg/: 4MB - audio allowed - max. 300 seconds"
 	echo -e "\\t\\t\\tall other boards: 3MB - no audio allowed - max. 120 seconds"
 	echo -e "\\t\\t8chan limits:"
 	echo -e "\\t\\t\\tall boards: 8MB - audio allowed"
-	echo -e "\\t-f filters: Add filters that you want to apply (with settings). DO NOT USE SCALING AS IT'S APPLIED AUTOMATICALLY RIGHT NOW! Also be careful to type them as you would normally with ffmpeg. Refer to ffmpeg's documentation for further information."
+	echo -e "\\t-f filters: Add filters that you want to apply (with settings). Be careful to type them as you would normally with ffmpeg. Refer to ffmpeg's documentation for further information."
 }
 
-# Use ffprobe to get video properties for later calculations
+# Looks for certain substrings $2 within a string $1
+# Succeeds if substring is found within the string (e.i. it contains the substring)
+contains () {
+	case "$1" in 
+		*"$2"*) return 0;;
+		*) return 1;;
+	esac
+}
+
+# Uses ffprobe to get video properties for later calculations
 info () {
 	length=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$1")
 	frame_rate=$(ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of default=noprint_wrappers=1:nokey=1 "$1")
@@ -28,7 +38,7 @@ info () {
 	aspect_ratio=$(bc <<< "scale=3; $video_width/$video_height")
 }
 
-# Trim function when flag is accordingly set
+# Asks user where to start and end each video and use it to calculate the video's duration
 trim () {
 	echo "Current file: $1"
 	echo "Please specify where to start encoding (in seconds):"
@@ -38,20 +48,25 @@ trim () {
 	duration=$(bc <<< "scale=3; $end_time-$start_time")
 }
 
-# Audio function when flag is accordingly set
+# Defines audio codec and properties
 audio () {
 	audio_bitrate=96
-	audio_settings="-c:a libvorbis -ac 2 -ar 44100 -b:a ${audio_bitrate}K"
+	if [[ "$new_codecs" = true ]]; then
+		audio_settings="-c:a libopus -ac 2 -ar 48000 -b:a ${audio_bitrate}K"
+		# -ar 48000, because Opus only allows certain sampling rates (48000, 24000, 16000, 12000, 8000)
+	else
+		audio_settings="-c:a libvorbis -ac 2 -ar 44100 -b:a ${audio_bitrate}K"
+	fi
 }
 
-# Function to calculate important values for the encoding process (mainly bitrate)
+# Calculates important values for the encoding process
 calc () {
 	video_bitrate=$(bc <<< "($file_size*8*1000/$duration-$audio_bitrate+0.5)/1")
 	(( bufsize = $video_bitrate*5 ))
 	bpp=$(bc <<< "scale=3; $video_bitrate*1000/($video_height*$video_width*$frame_rate)")
 }
 
-# Automatic downscale function
+# Reduces output height (width is later adjusted via aspect ratio) to reach certain bits per pixel value; min. output height is 360p
 downscale () {
 	while [[ $(bc <<< "$bpp < 0.04") -eq 1 && $video_height -gt 360 ]]; do
 		(( video_height -= 10 ))
@@ -60,30 +75,31 @@ downscale () {
 	done
 }
 
-# Bitrade mode function based on which mode was chosen by the user. Defaults to variable.
-mode () {
+# Defines which video codec and bitrate mode to use
+video () {
+	if [[ "$new_codecs" = true ]]; then video_codec="libvpx-vp9"; else video_codec="libvpx"; fi
 	# modes=( "variable" "low-variable" "constant" "skip_threshold")
 	case $1 in
-		1) video_settings="-crf 10 -qmax 50 -b:v ${video_bitrate}K";;
-		2) video_settings="-crf 10 -b:v ${video_bitrate}K";;		
-		3) video_settings="-minrate:v ${video_bitrate}K -maxrate:v ${video_bitrate}K -b:v ${video_bitrate}K";;
-		4) video_settings="-minrate:v ${video_bitrate}K -maxrate:v ${video_bitrate}K -b:v ${video_bitrate}K -skip_threshold 100";;
+		1) video_settings="-c:v $video_codec -crf 10 -qmax 50 -b:v ${video_bitrate}K";;
+		2) video_settings="-c:v $video_codec -crf 10 -b:v ${video_bitrate}K";;		
+		3) video_settings="-c:v $video_codec -minrate:v ${video_bitrate}K -maxrate:v ${video_bitrate}K -b:v ${video_bitrate}K";;
+		4) video_settings="-c:v $video_codec -bufsize $bufsize -minrate:v ${video_bitrate}K -maxrate:v ${video_bitrate}K -b:v ${video_bitrate}K -skip_threshold 100";;
 		*) echo "File still doesn't fit the specified limit. Please use ffmpeg manually.";;
 	esac
 }
 
-# Function for the actual conversion via ffmpeg
+# The actual ffmpeg conversion commands
 convert () {
 	if [[ $(bc <<< "$bpp >= 0.075") -eq 1 ]]; then
-		ffmpeg -y -ss $start_time -i "$1" -t $duration -c:v libvpx -slices 8 -threads 1 $video_settings -deadline good -cpu-used 5 $audio_settings -pass 1 -f webm /dev/null
-		ffmpeg -y -ss $start_time -i "$1" -t $duration -c:v libvpx -slices 8 -threads 1 -metadata title="${1%.*}" -auto-alt-ref 1 -lag-in-frames 16 -bufsize $bufsize $video_settings -deadline best -cpu-used 0 $scaling_factor $filter_settings $audio_settings -pass 2 "../done/${1%.*}.webm"
+		ffmpeg -y -ss $start_time -i "$1" -t $duration $video_settings -slices 8 -threads 1 -deadline good -cpu-used 5 $audio_settings -pass 1 -f webm /dev/null
+		ffmpeg -y -ss $start_time -i "$1" -t $duration $video_settings -slices 8 -threads 1 -metadata title="${1%.*}" -auto-alt-ref 1 -lag-in-frames 16 -deadline good -cpu-used 0 $scaling_factor $filter_settings $audio_settings -pass 2 "../done/${1%.*}.webm"
 		rm ffmpeg2pass-0.log
 	else
-		ffmpeg -y -ss $start_time -i "$1" -t $duration -c:v libvpx -slices 8 -threads 1 -metadata title="${1%.*}" -lag-in-frames 16 -bufsize $bufsize $video_settings -deadline best -cpu-used 0 $scaling_factor $filter_settings $audio_settings "../done/${1%.*}.webm"
+		ffmpeg -y -ss $start_time -i "$1" -t $duration $video_settings -slices 8 -threads 1 -metadata title="${1%.*}" -lag-in-frames 16 -deadline good -cpu-used 0 $scaling_factor $filter_settings $audio_settings "../done/${1%.*}.webm"
 	fi
 }
 
-# Function to swap bitrate mode if result is still over the file size limit
+# Loops through the different bitrate modes if output webm is too large
 limiter () {
 	webm_size=$(ffprobe -v error -show_entries format=size -of default=noprint_wrappers=1:nokey=1 "../done/${1%.*}.webm")
 	counter=1
@@ -103,26 +119,27 @@ limiter () {
 ###### Main script ######
 ####################
 
-# Reads input parameters and assigns values accordingly
-while getopts ":htas:f:" ARG; do
+# Read input parameters and assigns values accordingly
+while getopts ":htans:f:" ARG; do
 	case "$ARG" in
 	h) usage && exit;;
 	t) trim_mode=true;;
 	a) audio_mode=true;;
+	n) new_codecs=true;;
 	s) file_size=$OPTARG;;
 	f) filter_settings="-filter_complex $OPTARG";;
-	*) echo "Unknown flag used. Enter sh $0 -h to show all available options." && exit;;
+	*) echo "Unknown flag used. Use $0 -h to show all available options." && exit;;
 	esac;
 done
 
 # Set default values for unspecified parameters
 [[ -z $trim_mode ]] && trim_mode=false
 [[ -z $audio_mode ]] && audio_mode=false
+[[ -z $new_codecs ]] && new_codecs=false
 [[ -z $file_size ]] && file_size=3
 
 # Create sub-directory for the finished webms
 mkdir done
-
 # Change into sub-directory to avoid file conflicts when converting webms
 cd to_convert || exit
 
@@ -140,11 +157,11 @@ for input in *; do (
 	if [[ "$trim_mode" = true ]]; then trim "$input"; fi
 	if [[ "$audio_mode" = true ]]; then audio; fi
 	calc
-	downscale
-	mode 1
+	contains "$filter_settings" "scale" || downscale
+	video 1
 	convert "$input"
 	limiter "$input"
-
+	
 	# Print various variables for debugging purposes
 	#~ echo "Duration: $duration"
 	#~ echo "Frame rate: $frame_rate"
